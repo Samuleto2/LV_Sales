4cd00727c39773ab24cee0c8ed1c850895c4fab1
samfalco <samuleto2gmail.com>
2026-01-23 17:24:18 -0300

    remove dotenv import

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template
 from flask_login import login_required
 from app.config import Config
 from app.extensions import db, migrate, cors, login_manager
-from dotenv import load_dotenv
 
-load_dotenv() 
 
 def create_app():
     
--- app/config.py
+++ app/config.py
@@ -3,9 +3,12 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     # CRÍTICO: Cambiar en producción
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-key-CAMBIAR-EN-PRODUCCION"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    if not SECRET_KEY:
+        raise RuntimeError("SECRET_KEY no está configurada")
     
     DATABASE_URL = os.environ.get("DATABASE_URL")
     if not DATABASE_URL:
@@ -17,7 +20,7 @@ class Config:
     CORS_RESOURCES = {r"/*": {"origins": "*"}}
     
     # Configuración de sesión
-    SESSION_COOKIE_SECURE = True  # Solo HTTPS en producción
+    SESSION_COOKIE_SECURE = os.getenv("ENV") == "production"
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     PERMANENT_SESSION_LIFETIME = 21600  # 6hs
